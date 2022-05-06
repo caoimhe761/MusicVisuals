cf3d7c39387f86191b4559a798ec49b20a2e510e
caoimhe761 <98536140+caoimhe761@users.noreply.github.com>
2022-05-06 16:16:12 +0100

    Add files via upload

---
+++ java/src/ie/tudublin/Main.java
@@ -0,0 +1,18 @@
+package ie.tudublin;
+
+public class Main
+{
+    public static void audio4()
+	{
+		String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new Audio4());
+    }
+   
+    
+    public static void main(String[] args)
+    {
+        audio4();
+
+
+    }
+}
